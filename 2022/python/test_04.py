ddf7d21ecb5de562a280fc08a68aeafd4f2e6c63
Gustavo Picon <tabo@tabo.pe>
2022-12-03 22:04:23 -0800

    day4 + pythob

--- 2022/python/test_04.py
+++ 2022/python/test_04.py
@@ -2,23 +2,37 @@ import unittest
 from aoc_helpers import input_lines, example_lines
 
 
+def rangeset(s: str) -> set[int]:
+    n1, n2 = s.split("-")
+    return set(range(int(n1), int(n2) + 1))
+
+
+def pair_of_number_sets(ln: str) -> tuple[set[int], set[int]]:
+    p1, p2 = ln.split(",")
+    return rangeset(p1), rangeset(p2)
+
+
+def lines_to_pairs(lns: list[str]) -> iter[tuple[set[int], set[int]]]:
+    return [pair_of_number_sets(ln) for ln in lns]
+
+
 def part1(lns: list[str]) -> int:
-    return len(lns)
+    return len([1 for v1, v2 in lines_to_pairs(lns) if v1 >= v2 or v2 >= v1])
 
 
 def part2(lns: list[str]) -> int:
-    return len(lns)
+    return len([1 for v1, v2 in lines_to_pairs(lns) if v1 & v2])
 
 
 class Day04(unittest.TestCase):
     def test_part1_example(self):
-        self.assertEqual(0, part1(example_lines(day=4)))
+        self.assertEqual(2, part1(example_lines(day=4)))
 
     def test_part2_example(self):
-        self.assertEqual(0, part2(example_lines(day=4)))
+        self.assertEqual(4, part2(example_lines(day=4)))
 
     def test_part1_solution(self):
-        self.assertEqual(0, part1(input_lines(day=4)))
+        self.assertEqual(550, part1(input_lines(day=4)))
 
     def test_part2_solution(self):
-        self.assertEqual(0, part2(input_lines(day=4)))
+        self.assertEqual(931, part2(input_lines(day=4)))
